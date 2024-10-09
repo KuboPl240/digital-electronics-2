452a08a543dded0da77de246390286e0d6462742
KuboPl240 <240967@vutbr.cz>
2024-10-09 17:35:58 +0200

    final

---
+++ lab3-interrupts/timers/src/main.c
@@ -0,0 +1,52 @@
+#include <avr/io.h>         // AVR device-specific IO definitions
+#include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
+#include <gpio.h>           // GPIO library for AVR-GCC
+#include <timer.h>          // Timer library for AVR-GCC
+
+
+/* Defines ------------------------------------------------*/
+#define LED_GREEN PB5  // Arduino Uno on-board LED
+#define LED_RED PB0    // External active-low LED
+
+
+/* Function definitions -----------------------------------*/
+/***********************************************************
+ * Function: Main function where the program execution begins
+ * Purpose:  Toggle two LEDs using the internal 8- and 16-bit 
+ *           Timer/Counter.
+ * Returns:  none
+ ***********************************************************/
+int main(void)
+{
+    // Set pins where LEDs are connected as output
+    GPIO_mode_output(&DDRB, LED_GREEN);
+
+    // Configuration of 16-bit Timer/Counter1 for LED blinking
+    // Set the overflow prescaler to 262 ms and enable interrupt
+    TIM1_ovf_262ms();
+    TIM1_ovf_enable();
+
+    // Enables interrupts by setting the global interrupt mask
+    sei();
+
+    // Infinite loop
+    while (1)
+    {
+        /* Empty loop. All subsequent operations are performed exclusively 
+         * inside interrupt service routines, ISRs */
+    }
+
+    // Will never reach this
+    return 0;
+}
+
+
+/* Interrupt service routines -----------------------------*/
+/***********************************************************
+ * Function: Timer/Counter1 overflow interrupt
+ * Purpose:  Toggle on-board LED.
+ ***********************************************************/
+ISR(TIMER1_OVF_vect)
+{
+    GPIO_toggle(&PORTB, LED_GREEN);
+}
